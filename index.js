4885d62d6e827c99b25e43027233c27c0cb7249d
tomaslb26 <tomaslb26@gmail.com>
2022-11-23 16:54:16 +0000

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 8081;
 
 app.get('/', async (req, res) => {
